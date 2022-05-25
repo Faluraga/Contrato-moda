c589a99e1e5ec1141ad89e25bc11d47065151bbf
Faluraga <faluraga2021@gmail.com>
2022-05-25 15:53:04 -0500

    two commit

--- camera.js
+++ camera.js
@@ -1,22 +1,16 @@
-console.log("Francisco Ramirez papito bello ");
-var take_input = document.getElementById("take_input")
-var save_value = document.getElementById("save_value")
-var localstorage_value = document.getElementById("localstorage_value")
-var read_localstorage = document.getElementById("read_localstorage")
-var delete_data = document.getElementById("delete_data")
+$("input").change(function(e) {
 
-
-save_value.onclick = function(){
-    localStorage.setItem("name" ,take_input.value);
-}
-
-read_localstorage.onclick = function(){
-    localstorage_value.textContent = localStorage.getItem("name")
-    localstorage_value.addEventListener =localStorage.getElementById("name")
-}
-
-delete_data.onclick = function(){
-    localStorage.removeItem("name")
-}
-
-
+    for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
+        
+        var file = e.originalEvent.srcElement.files[i];
+        
+        var img = document.createElement("img");
+        var reader = new FileReader();
+        reader.onloadend = function() {
+             img.src = reader.result;
+        }
+        reader.readAsDataURL(file);
+        $("input").after(img);
+    }
+  });
+  
